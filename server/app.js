b85f2532bbd15a2b77a623ab53a80ca5165cb899
piyush rai <piyushrai7271@gmail.com>
2025-08-26 12:10:56 +0530

    handling corse issue

--- server/app.js
+++ server/app.js
@@ -1,29 +1,24 @@
-import express from 'express';
-import cors from 'cors';
-import cookieParser from 'cookie-parser';
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
 app.use(
-    cors({
-        origin: process.env.CORS_ORIGIN,
-        credentials: true,
-    })
+  cors({
+    origin: process.env.CORS_ORIGIN || "http://localhost:5173",
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true,
+  })
 );
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 //Routes Import and handling
 import userRoutes from "./src/routes/user.routes.js";
 
-
-app.use("/api/user",userRoutes);
-
-
+app.use("/api/user", userRoutes);
 
 export default app;
-
-
-
